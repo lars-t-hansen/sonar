ad64462833e32ed2c3a14bd49c5e3b1a55e95e99
Lars T Hansen <larstha@uio.no>
2024-12-13 11:39:48 +0100

    Abstraction

---
+++ sandbox/nvidia/sonar-nvml.c
@@ -0,0 +1,123 @@
+#include <stddef.h>
+#include <inttypes.h>
+#include <dlfcn.h>
+
+/* Remember to `module load CUDA/11.1.1-GCC-10.2.0` or similar for nvml.h */
+#include <nvml.h>
+
+#include "sonar-nvml.h"
+
+static int load_nvml();
+static void unload_nvml();
+
+static nvmlReturn_t (*xnvmlInit)();
+static nvmlReturn_t (*xnvmlDeviceGetCount_v2)(unsigned*);
+static nvmlReturn_t (*xnvmlDeviceGetHandleByIndex_v2)(int index, nvmlDevice_t* dev);
+static nvmlReturn_t (*xnvmlDeviceGetArchitecture)(nvmlDevice_t, nvmlDeviceArchitecture_t*);
+static nvmlReturn_t (*xnvmlDeviceGetMemoryInfo)(nvmlDevice_t, nvmlMemory_t*);
+
+static int is_open;
+
+int nvml_open() {
+    if (load_nvml() != 0) {
+        return -1;
+    }
+    if (xnvmlInit() != 0) {
+        return -1;
+    }
+    is_open = 1;
+    return 0;
+}
+
+int nvml_close() {
+    if (!is_open) {
+        return -1;
+    }
+    unload_nvml();
+    is_open = 0;
+    return 0;
+}
+
+int nvml_device_get_count(uint32_t* count) {
+    if (!is_open) {
+        return -1;
+    }
+    unsigned ndev;
+    if (xnvmlDeviceGetCount_v2(&ndev) != 0) {
+        return -1;
+    }
+    *count = ndev;
+    return 0;
+}
+
+int nvml_device_get_architecture(uint32_t device, uint32_t* arch) {
+    if (!is_open) {
+        return -1;
+    }
+    nvmlDevice_t dev;
+    if (xnvmlDeviceGetHandleByIndex_v2(device, &dev) != 0) {
+        return -1;
+    }
+    nvmlDeviceArchitecture_t n_arch;
+    if (xnvmlDeviceGetArchitecture(dev, &n_arch) != 0) {
+        return -1;
+    }
+    *arch = n_arch;
+    return 0;
+}
+
+int nvml_device_get_memory_info(uint32_t device, uint64_t* total, uint64_t* used, uint64_t* free) {
+    if (!is_open) {
+        return -1;
+    }
+    nvmlDevice_t dev;
+    if (xnvmlDeviceGetHandleByIndex_v2(device, &dev) != 0) {
+        return -1;
+    }
+    nvmlMemory_t mem;
+    if (xnvmlDeviceGetMemoryInfo(dev, &mem) != 0) {
+        return -1;
+    }
+    *total = mem.total;
+    *used = mem.used;
+    *free = mem.free;
+    return 0;
+}
+
+// dynamic library management
+
+static void* lib;
+
+static void* lookup(const char* sym) {
+    if (lib == NULL) {
+        lib = dlopen("/usr/lib64/libnvidia-ml.so", RTLD_NOW);
+        if (lib == NULL) {
+            return NULL;
+        }
+    }
+    return dlsym(lib, sym);
+}
+
+static int load_nvml() {
+    if ((xnvmlInit = lookup("nvmlInit")) == NULL) {
+        return -1;
+    }
+    if ((xnvmlDeviceGetCount_v2 = lookup("nvmlDeviceGetCount_v2")) == NULL) {
+        return -1;
+    }
+    if ((xnvmlDeviceGetHandleByIndex_v2 = lookup("nvmlDeviceGetHandleByIndex_v2")) == NULL) {
+        return -1;
+    }
+    if ((xnvmlDeviceGetArchitecture = lookup("nvmlDeviceGetArchitecture")) == NULL) {
+        return -1;
+    }
+    if ((xnvmlDeviceGetMemoryInfo = lookup("nvmlDeviceGetMemoryInfo")) == NULL) {
+        return -1;
+    }
+    return 0;
+}
+
+static void unload_nvml() {
+    dlclose(lib);
+}
+
---
+++ sandbox/nvidia/sonar-nvml.h
@@ -0,0 +1,20 @@
+// API to the sonar NVML library
+
+/* These return 0 on success, -1 on any kind of error.  Results are always passed via 'out' parameters,
+   either in structure form or as broken-out fields.
+
+   The intent is that the AMD API should look pretty much the same, maybe even be identical.
+*/
+
+#include <inttypes.h>
+
+int nvml_open();
+int nvml_close();
+
+int nvml_device_get_count(uint32_t* count);
+
+/* The architecture is a well-defined number, see nvml.h */
+int nvml_device_get_architecture(uint32_t device, uint32_t* arch);
+
+/* The unit is 'bytes' */
+int nvml_device_get_memory_info(uint32_t device, uint64_t* total, uint64_t* used, uint64_t* free);
